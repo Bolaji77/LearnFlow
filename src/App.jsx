b2a489e8e3ca591a4086c0834d523adc8cc21c7d
Bolaji <akinloyebolaji7@gmail.com>
2025-08-13 04:49:40 -0700

    second setup commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,10 @@
-import  Login from "./routes/Login"
-import Landing from "./routes/Landing";
-import {Routes, Route, Navigate} from 'react-router-dom'
-import Signup from "./routes/Signup";
-import Navbar from "./components/Navbar";
-import { useAuth } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute"
-import Dashboard from "./routes/Dashboard"
-
+import React from "react";
 
 function App() {
-  const {loading, currentUser} = useAuth();
-
-  if(loading){
-    return (
-      <div className="min-h-screen bg-slate-50 flex items-center justify-center">
-        <div className="animate-pulse text-indigo-600 text-xl ">Loading QuickNotes...</div>
-      </div>
-    )
-  }
   return (
     <>
     <div className="min-h-screen bg-slate-50">
-    <Navbar />
-      <Routes>
-        <Route path="/" element={currentUser ? <Navigate to="/dashboard" replace /> : <Landing />} />
-
-        <Route path="/login" element={currentUser ? <Navigate to="/dashboard" replace /> : <Login />} />
-
-        <Route path="/signup" element={currentUser ? <Navigate to="/dashboard" replace /> : <Signup />} />
-
-        <Route path="/dashboard" element={
-          <ProtectedRoute >
-            <div className="container mx-auto px-4 py-8">
-              <Dashboard />
-            </div>
-          </ProtectedRoute>
-        }/>
-      </Routes>
+      LearnFlow
     </div>
       </>
   );
